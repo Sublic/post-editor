5ba261b9238adb5e3c4ae7cfe8298231c6d52e65
Alex Telegin <telegin.alexander@gmail.com>
2024-03-03 22:51:16 +0100

    Add token actions

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,5 +3,11 @@ export const GREEN_CHAIN_ID = parseInt(process.env.NEXT_PUBLIC_GREEN_CHAIN_ID!);
 export const FACTORY_ADDRESS =
   "0x825139E59954056A548d5B8765B015E88d0B5A23" as `0x${string}`;
 export const BUCKET_HUB_ADDRESS = process.env
-  .NEXT_PUBLIC_BUCKET_HUB_ADDRESS! as `0x${string}`;
+  .NEXT_PUBLIC_BUCKET_HUB_ADDRESS! as `0${string}`;
+export const TOKEN_FACTORY_ADDRESS = 
+  "0x1697A8D0dFeBE8b55399Fcc548EE6f833791F5E7" as `0x${string}`;
+  // process.env.NEXT_TOKEN_FACTORY_ADDRESS! as `0x${string}`;
+export const SWAPPER_ADDRESS = 
+  "0xD578a9A1409c467eb12Ff0F7A7b8224530551bA4" as `0x${string}`;
+  // process.env.NEXT_SWAPPER_ADDRESS! as `0x${string}`;
 export const WC_ID = process.env.NEXT_PUBLIC_WC_ID!;
