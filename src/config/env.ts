c39e8972c33689ac1223dad824f5ce260b6414ee
Serafim Gerasimov <serafimgerasimov44@gmail.com>
2024-03-04 12:38:06 +0300

    chore: contract address

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,7 @@
 export const GRPC_URL = process.env.NEXT_PUBLIC_GRPC_URL!;
 export const GREEN_CHAIN_ID = parseInt(process.env.NEXT_PUBLIC_GREEN_CHAIN_ID!);
 export const FACTORY_ADDRESS =
-  "0x27b3d8a080bce07ed16f89913220aC38205967B4" as `0x${string}`;
+  "0x825139E59954056A548d5B8765B015E88d0B5A23" as `0x${string}`;
 export const BUCKET_HUB_ADDRESS = process.env
   .NEXT_PUBLIC_BUCKET_HUB_ADDRESS! as `0x${string}`;
 export const WC_ID = process.env.NEXT_PUBLIC_WC_ID!;
