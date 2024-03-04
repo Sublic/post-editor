d79caf4a473744f95d08badc6a3c8a21c6c736d0
Serafim Gerasimov <serafimgerasimov44@gmail.com>
2024-03-04 17:33:29 +0300

    fix: hex type

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,7 @@ export const GREEN_CHAIN_ID = parseInt(process.env.NEXT_PUBLIC_GREEN_CHAIN_ID!);
 export const FACTORY_ADDRESS =
   "0xBE5a58A4Ba78eb5450B79E385eD8fEf94B9EE25c" as `0x${string}`;
 export const BUCKET_HUB_ADDRESS = process.env
-  .NEXT_PUBLIC_BUCKET_HUB_ADDRESS! as `0${string}`;
+  .NEXT_PUBLIC_BUCKET_HUB_ADDRESS! as `0x${string}`;
 export const TOKEN_FACTORY_ADDRESS =
   "0x1697A8D0dFeBE8b55399Fcc548EE6f833791F5E7" as `0x${string}`;
 // process.env.NEXT_TOKEN_FACTORY_ADDRESS! as `0x${string}`;
